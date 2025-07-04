b521de2be452d6bd94bef5dbf82aed2ddac3813c
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2025-07-04 18:30:18 +0500

    added .env and updated axios instance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+console.log("Backend URL:", BACKEND_URL);
 export const axiosInstance = axios.create({
     baseURL: BACKEND_URL, 
     withCredentials: true,
